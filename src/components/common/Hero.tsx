6bb48ebde6e5a828677ac0461a414c530681ec11
David Tribugenia <david.estrelloso.tribugenia@gmail.com>
2024-07-15 07:58:10 +0800

    responsive landing

--- src/components/common/Hero.tsx
+++ src/components/common/Hero.tsx
@@ -34,25 +34,25 @@ const Hero = () => {
     },
   ];
   return (
-    <main className="min-h-[calc(100vh-4rem)] bg-gradient-to-b to-[#624ff62d] from-[#FFFFFF]">
+    <main className="min-h-[calc(100vh-4rem)] bg-gradient-to-b from-[#624ff62a] to-[#624ff615]">
       <Container className="px-4">
         <div className="mt-8">
           <div className="flex flex-col sm:flex-row gap-2 sm:gap-0 items-center">
-            <h1 className="sm:text-4xl text-2xl font-bold text-center sm:text-start">
+            <h1 className="sm:text-4xl text-3xl font-bold text-center sm:text-start">
               Streamline Your Insurance Process with Our{" "}
-              <span className="sm:text-4xl text-2xl text-primary">
+              <span className="sm:text-4xl text-3xl text-primary">
                 AI-Powered
               </span>{" "}
               Chatbot
             </h1>
 
-            <div className="flex items-center gap-x-4 min-w-[326px] justify-center sm:justify-start">
+            <div className="flex mt-2 sm:mt-0 sm:flex-col lg:flex-row sm:items-end lg:items-center gap-x-4 min-w-[326px] justify-center sm:justify-start">
               <div className="flex items-start">
                 {avatar.map((item, index) => (
                   <img
                     key={`key-img-${index}`}
                     className={twMerge(
-                      "-ml-2 sm:-ml-3 w-[33px] h-[33px] sm:w-[55px] sm:h-[55px]",
+                      "-ml-2 sm:-ml-3 w-[50px] h-[50px] sm:w-[55px] sm:h-[55px]",
                       index === 0 && "ml-0"
                     )}
                     src={item}
@@ -61,19 +61,21 @@ const Hero = () => {
                 ))}
               </div>
               <div className="flex flex-col">
-                <h1 className="text-md sm:text-2xl font-extrabold">750k+</h1>
+                <h1 className="text-md sm:text-2xl font-extrabold sm:text-end lg:text-start">
+                  750k+
+                </h1>
                 <p className="text-nowrap text-[#706571]">Happy customers</p>
               </div>
             </div>
           </div>
-          <div className="flex items-center justify-between">
-            <p className="text-[#706571]">
+          <div className="mt-4 md:mt-0 flex flex-col gap-y-2 sm:gap-y-0 sm:flex-row items-center justify-center sm:justify-between">
+            <p className="text-[#706571] text-center sm:text-start">
               Automate, simplify, and enhance your client interactions
               effortlessly
             </p>
-            <div className="flex gap-x-4">
+            <div className="flex gap-x-4 w-full sm:w-auto">
               <Button
-                className="flex items-center bg-primary text-white w-[183px] justify-center py-3"
+                className="flex items-center bg-primary text-white w-full sm:w-[183px] justify-center py-3"
                 onClick={() => {
                   window.location.href = "/sign-in";
                 }}
@@ -87,14 +89,19 @@ const Hero = () => {
               </Button>
             </div>
           </div>
-          <div className="flex items-start mt-8 overflow-hidden gap-x-4 justif">
+          <div className="flex flex-col sm:h-[400px] sm:flex-row mt-8 overflow-hidden gap-4 rounded-[40px]">
             <img
-              src="/images/heroFinal.png"
-              alt=""
-              className="object-cover w-full"
+              src="/images/hero1.png"
+              alt="hero1"
+              className="flex-1 object-cover rounded-[40px]"
+            />
+            <img
+              src="/images/hero2.png"
+              alt="hero1"
+              className="flex-1 object-cover rounded-[40px]"
             />
           </div>
-          <div className="flex justify-center w-full py-8 mt-8 bg-white shadow-lg gap-x-6 rounded-2xl">
+          <div className="flex justify-center flex-wrap w-full px-4 gap-y-2 py-8 mt-8 bg-white shadow-lg gap-x-6 rounded-2xl">
             {company.map((item, index) => (
               <span
                 key={`key-company-${index}`}
