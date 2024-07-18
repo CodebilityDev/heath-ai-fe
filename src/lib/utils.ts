cbdf61be563a615c2867c3c0036c9f4dae0e4014
David Tribugenia <david.estrelloso.tribugenia@gmail.com>
2024-07-18 12:18:47 +0800

    convert signup and signin forms to shadcn forms for validation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { z } from "zod";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function passwordValidator(password: string) {
+  const uppercaseRegex = /[A-Z]/;
+  if (!uppercaseRegex.test(password)) {
+    return false;
+  }
+
+  const lowercaseRegex = /[a-z]/;
+  if (!lowercaseRegex.test(password)) {
+    return false;
+  }
+
+  const digitRegex = /\d/;
+  if (!digitRegex.test(password)) {
+    return false;
+  }
+
+  const symbolRegex = /[!@#$%^&*()_+\-=\[\]{}|;':"\\,.\/?]+/;
+  if (!symbolRegex.test(password)) {
+    return false;
+  }
+  return true;
 }
